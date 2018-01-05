052a3d5196a422a3f99d3c926a16bc8bc75cd9c0
Yicong-Huang <hyc541978023@gmail.com>
2018-01-04 17:38:50 -0800

    basic skeleton version 0.1.0

---
+++ app/views.py
@@ -0,0 +1,62 @@
+from flask import render_template, request
+
+from app import app
+from app.models import Restaurant
+
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html', name="hello")
+
+
+@app.route('/dev')
+def index():
+    RCs = Restaurant.query.all()
+    entries = [dict(
+        name=r.name,
+        imageurl=r.imageurl,
+        phone=phone_display(r.phone),
+        rating=rating_display(r.rating),
+        cuisine=r.cuisine,
+        subcategory=r.subcategory
+    ) for r in RCs]
+    return render_template('index.html', name="develop page", entries=entries)
+
+
+# @app.route('/search', method=['GET', 'POST'])
+# def search():
+#     pass
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    pattern = request.args.get("q")
+
+    RCs = list()
+    if pattern:
+        RCs.extend(Restaurant.query.filter(Restaurant.name.like("%" + pattern + "%")).all())
+        RCs.extend(Restaurant.query.filter(Restaurant.cuisine.like("%" + pattern + "%")).all())
+        RCs.extend(Restaurant.query.filter(Restaurant.subcategory.like("%" + pattern + "%")).all())
+        RCs.extend(Restaurant.query.filter(Restaurant.city.like("%" + pattern + "%")).all())
+
+        entries = [dict(
+            name=r.name,
+            imageurl=r.imageurl,
+            phone=phone_display(r.phone),
+            rating=rating_display(r.rating),
+            cuisine=r.cuisine,
+            subcategory=r.subcategory
+        ) for r in RCs]
+
+    return render_template('index.html', entries=entries)
+
+
+def phone_display(phone: str) -> str:
+    if phone:
+        return "(" + phone[:3] + ")-" + phone[3:6] + "-" + phone[6:]
+    else:
+        return ""
+
+
+def rating_display(rating: float):
+    return {'integer': range(int(rating)), 'decimal': 0 if (rating - int(rating)) < 0.5 else 1,
+            'empty': range(5 - int(rating) - (0 if (rating - int(rating)) < 0.5 else 1))}
---
+++ config.py
@@ -0,0 +1,2 @@
+SQLALCHEMY_DATABASE_URI = 'mysql://root:520965huang@utilities-db.coryhpm7bqn1.us-east-1.rds.amazonaws.com/kerena'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
